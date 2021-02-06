47a642c38e5510e90a9cb345a8c9eed5a2a7dc5a
customcommander <hello@spinjs.com>
2021-02-06 20:59:38 +0000

    feat: add support for description, parameters and return value
    parameters documentation is still limited.
    for example it doesn't include description and default value yet.

--- src/inc.js
+++ src/inc.js
@@ -6,7 +6,17 @@
 /** @namespace / */
 
 /**
+ * Adds 1 to `x`.
+ *
+ * @example
+ * ```javascript
+ * inc(41);
+ * //=> 42
+ * ```
+ *
  * @public
  * @function inc
+ * @param {number} x
+ * @return {number}
  */
 module.exports = x => x + 1;
